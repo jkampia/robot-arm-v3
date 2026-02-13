718974a90161cdf0525a6278d3f4139815c2fe29
jkampia <jonathankampia@gmail.com>
2026-02-13 01:57:25 -0500

    some random changes

---
+++ teensy/print_utils.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+/* print helpers */
+
+inline void printArray(const String& msg, const float* array, int len)
+{
+  Serial.print(msg);
+  Serial.print("[ ");
+  for (int i = 0; i < len-1; i++) 
+  {
+    Serial.print(array[i]);
+    Serial.print(", ");
+  }
+  Serial.print(array[len-1]);
+  Serial.print(" ]\n");
+}
+
+inline void printArray(const String& msg, const int * array, int len)
+{
+  Serial.print(msg);
+  Serial.print("[ ");
+  for (int i = 0; i < len-1; i++) 
+  {
+    Serial.print(array[i]);
+    Serial.print(", ");
+  }
+  Serial.print(array[len-1]);
+  Serial.print(" ]\n");
+}
+
+inline void printArray(const String& msg, const bool * array, int len)
+{
+  Serial.print(msg);
+  Serial.print("[ ");
+  for (int i = 0; i < len-1; i++) 
+  {
+    Serial.print(array[i]);
+    Serial.print(", ");
+  }
+  Serial.print(array[len-1]);
+  Serial.print(" ]\n");
+}
+
